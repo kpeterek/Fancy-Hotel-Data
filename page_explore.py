9ae98d47880e2f2ff42dc5dfcf66550727db86c9
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-27 12:59:12 -0800

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -24,11 +24,27 @@ def page_explore():
                 return st.sidebar.beta_expander(expander_name)
     
     st.sidebar.subheader("To explore:")
-    with make_expanders("Select distribution"):
+    with make_expanders("Select Hotel"):
     
 
         # Distribution names
-        display = distr_selectbox_names()
+        hotel = st.sidebar.selectbox('Select Hotel',name_str['Property'])
+		st.sidebar.write(hotel, ' has the StarID of ',name_str[name_str.Property == hotel]['StarID'].item())
+		data = dodge_pipeline[['Title','Address','City','State','PostalCode','Units','Open Date','Phase','Latitude','Longitude']]
+		star = st.sidebar.text_input('Enter Star ID')
+		st_filter = st.sidebar.selectbox('Filter by?', ['radius','tract','city'])
+		radius = st.sidebar.text_input('Radius?')
+		if radius.isnumeric():
+    			pass
+		else:
+    			radius = 0.0
+		submit = st.sidebar.button('run new supply')
+		if submit:
+			data = dd.newsupply(float(star),float(radius),st_filter)
+			st.write(data.dropna())
+			data.rename(columns = {'Latitude':'lat','Longitude':'lon'},inplace=True)
+			data.dropna(inplace=True)
+			st.map(data)
         
         # Create select box widget containing all SciPy function
         select_distribution = st.selectbox(
