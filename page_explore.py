99d9d0d110d8d2003b11b79f150a04c0b69beec9
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-27 12:20:13 -0800

    Create page_explore.py

---
+++ page_explore.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from scipy import stats
+from scipy.stats.mstats import mquantiles
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import base64
+
+
+def page_explore():
+    """ 
+    The first page in this app made with Streamlit is for an interactive 
+    exploration of the continuous distributions that are available in SciPy.
+    """
+    
+    name_docstring_dict, name_eq_dict, name_proper_dict, \
+        all_dist_params_dict, name_url_dict = creating_dictionaries()
+    
+    def make_expanders(expander_name, sidebar=True):
+        """ Set up expanders which contains a set of options. """
+        if sidebar:         
+            try:
+                return st.sidebar.expander(expander_name)
+            except:
+                return st.sidebar.beta_expander(expander_name)
+    
+    st.sidebar.subheader("To explore:")
+    with make_expanders("Select distribution"):
+    
+
+        # Distribution names
+        display = distr_selectbox_names()
+        
+        # Create select box widget containing all SciPy function
+        select_distribution = st.selectbox(
+             'Click below (or type) to choose a distribution',
+             display)
+        
+        st.markdown("**Parameters**")
