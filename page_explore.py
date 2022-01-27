d32946f74ad7b13df7ae30946e90ff8c4bfe05ee
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-27 14:00:17 -0800

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -33,7 +33,7 @@ def page_explore():
 
 	hotel = st.sidebar.selectbox('Select Hotel',name_str['Hotel Name'])
 	st.sidebar.write(hotel, ' has the StarID of ',name_str[name_str['Hotel Name'] == hotel]['STR Number'].item())
-	star = st.sidebar.text_input('Enter Star ID')
+	star = st.sidebar.text_input('Enter Star ID',default=63073)
 	st.markdown("**Parameters**")	
 	submit = st.sidebar.button('Pull Hotel Information')
 	data = str_census[str_census['STR Number'] == int(star)]
