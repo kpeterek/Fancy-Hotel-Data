a3368a2d038670509dd3be851405d56850e2437c
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-28 13:25:36 -0800

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -103,7 +103,7 @@ def page_explore():
 				)
 			)
 
-		st.plotly_chart(fig)
+		st.write(st.plotly_chart(fig)).transpose()
 	coords = list(data.T[['Latitude','Longitude']].values.flatten())
 	m = folium.Map(location=coords, zoom_start=16)
 	tooltip = data.T['Hotel Name'].values[0]
