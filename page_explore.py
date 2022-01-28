3a96423168efa2bcc8c815c24510e331704e910a
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-27 22:41:21 -0600

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -58,7 +58,7 @@ def page_explore():
 	#tooltip = data['Hotel Name'].values[0]
 	#folium.Marker(data[['Latitude','Longitude']].get_values()[0], tooltip=tooltip).add_to(m)
 	#folium_static(m)
-	st.write(list(data['Latitude'].values[0],data'Longitude'].values[0])
+	st.write(data[['Latitude','Longitude']].values.tolist().flatten())
 	radius_option = st.sidebar.radio("Options", ('7mile search radius', 'Custom Slider','Manual Input')) 
 	
 
