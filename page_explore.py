156c180652444a993a280072a45713a125a70042
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-28 17:30:53 -0600

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -31,7 +31,7 @@ def page_explore():
 	name_str = str_census[['Hotel Name','STR Number']]
     	
 	
-	row_1_1,row_2_1 = st.columns([4,1])
+	row_1_1,row_2_1 = st.columns([1,4])
 	def map(data, lat, lon, zoom):
 		st.write(pdk.Deck(
 		map_style="mapbox://styles/mapbox/light-v9",
