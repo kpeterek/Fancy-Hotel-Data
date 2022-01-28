b3164f4a522566072786c01bc67b46bc5951e34b
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-28 17:26:04 -0600

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -79,13 +79,22 @@ def page_explore():
 	hotel = st.sidebar.selectbox('Select Hotel',name_str['Hotel Name'])
 	st.sidebar.write(hotel, ' has the StarID of ',name_str[name_str['Hotel Name'] == hotel]['STR Number'].item())
 	star = st.sidebar.number_input(label='Enter Star ID',value=63037)
-	
+	fig = go.Figure(data=[go.Table(
+	    header=dict(values=list(data[cols_needed].columns),
+			fill_color='paleturquoise',
+			align='left'),
+	    cells=dict(values=data[cols_needed].transpose().values.tolist(),
+		       fill_color='lavender',
+		       align='left'))
+	])
+
+	fig.show()
 	
 	with row_1_1:
 		st.markdown("**Subject Property**")	
 		submit = st.sidebar.button('Pull Hotel Information')
 		data = str_census[str_census['STR Number'] == int(star)]
-		st.write(data[cols_needed].T)
+		st.plotly_chart(fig)
 
 	with row_2_1:
 		coords = list(data[['Latitude','Longitude']].values.flatten())
